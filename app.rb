085bbff146f9d1f76ea2fbc6c8c65387df80c0b1
Jeremy Allard <jeremy@allard.io>
2015-02-11 05:50:08 +0000

    responsive menu layout :facepunch:

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra/base'
+require 'sinatra/assetpack'
+require 'haml'
+require 'sass'
+
+class App < Sinatra::Base
+  register Sinatra::AssetPack
+  assets do
+    css :application, [
+          "/css/app.css"
+        ]
+    css_compression :sass
+  end
+  
+  get "/" do
+    haml :index
+  end
+
+  get "/platforms/rural" do
+    haml :rural
+  end
+
+  run! if app_file == $0
+end
